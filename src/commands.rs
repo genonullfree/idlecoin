5f019e098c22997c1f657b0940311e4f7b03fe65
geno <nullfree.geno@gmail.com>
2022-04-02 23:29:53 -0400

    Updating purchasing

--- src/commands.rs
+++ src/commands.rs
@@ -24,66 +24,95 @@ pub fn read_inputs(
             );
         }
 
+        let mut new_boost = 0;
+        let mut new_boost_cost = 0u128;
+        let mut new_miners = 0;
+        let mut new_miners_cost = 0u128;
+
         for i in buf[..len].iter() {
-            // TODO: Rework buying
-            if *i == 98 {
-                // 'b'
+            if *i == b'b' {
                 let mut wals = wallets.lock().unwrap();
                 for w in wals.iter_mut() {
                     if w.id == c.miner.wallet_id {
-                        if w.idlecoin < 1024 && w.supercoin < 1 {
-                            c.updates.push(
-                                "You need at least 1024 idlecoin to be able to purchase boost\n"
-                                    .to_string(),
-                            );
-                            continue;
+                        let cost = buy_boost(c, w);
+                        if cost > 0 {
+                            new_boost += 128;
+                            new_boost_cost += cost as u128;
                         }
-                        let cost = 1024u64;
-                        let boost = 128u64;
-                        let t: DateTime<Local> = Local::now();
-                        msg.insert(
-                            0,
-                            format!(
-                                " [{}] Miner 0x{:08x} bought {} boost seconds with {} idlecoin\n",
-                                t.format("%Y-%m-%d %H:%M:%S"),
-                                c.miner.miner_id,
-                                boost,
-                                cost
-                            ),
-                        );
-                        miner::sub_idlecoins(w, cost);
-                        c.miner.boost = c.miner.boost.saturating_add(boost);
                     }
                 }
                 drop(wals);
             }
-            if *i == 109 {
-                // 'm'
+            if *i == b'm' {
                 let mut wals = wallets.lock().unwrap();
                 for w in wals.iter_mut() {
                     if w.id == c.miner.wallet_id {
-                        if w.max_miners >= 10 {
-                            c.updates
-                                .push("You cannot purchase any more miners\n".to_string());
-                            continue;
-                        }
-                        let cost = u64::MAX / (100000 >> (w.max_miners - 5));
-                        if w.idlecoin > cost || w.supercoin > 0 {
-                            let t: DateTime<Local> = Local::now();
-                            msg.insert(0, format!(" [{}] Wallet 0x{:016x} bought a new miner license with {} idlecoin\n", t.format("%Y-%m-%d %H:%M:%S"), c.miner.wallet_id, cost));
-                            miner::sub_idlecoins(w, cost);
-                            w.max_miners += 1;
-                        } else {
-                            c.updates.push(format!(
-                                "You need {} idlecoin to purchase another miner license\n",
-                                cost
-                            ));
+                        let cost = buy_miner(c, w);
+                        if cost > 0 {
+                            new_miners += 1;
+                            new_miners_cost += cost as u128;
                         }
                     }
                 }
                 drop(wals);
             }
         }
+        let t: DateTime<Local> = Local::now();
+        if new_boost > 0 {
+            msg.insert(
+                0,
+                format!(
+                    " [{}] Miner 0x{:08x} bought {} boost seconds with {} idlecoin\n",
+                    t.format("%Y-%m-%d %H:%M:%S"),
+                    c.miner.miner_id,
+                    new_boost,
+                    new_boost_cost,
+                ),
+            );
+        }
+        if new_miners > 0 {
+            msg.insert(
+                0,
+                format!(
+                    " [{}] Wallet 0x{:016x} bought a new miner license with {} idlecoin\n",
+                    t.format("%Y-%m-%d %H:%M:%S"),
+                    c.miner.wallet_id,
+                    new_miners_cost
+                ),
+            );
+        }
     }
     drop(cons);
+}
+
+fn buy_boost(connection: &mut Connection, wallet: &mut Wallet) -> u64 {
+    if wallet.idlecoin < 1024 && wallet.supercoin < 1 {
+        connection
+            .updates
+            .push("You need at least 1024 idlecoin to be able to purchase boost\n".to_string());
+        return 0;
+    }
+    let cost = 1024u64;
+    let boost = 128u64;
+    miner::sub_idlecoins(wallet, cost);
+    connection.miner.boost = connection.miner.boost.saturating_add(boost);
+
+    1024
+}
+
+fn buy_miner(connection: &mut Connection, mut wallet: &mut Wallet) -> u64 {
+    if wallet.max_miners >= 10 {
+        connection
+            .updates
+            .push("You cannot purchase any more miners\n".to_string());
+        return 0;
+    }
+
+    let cost = u64::MAX / (100000 >> (wallet.max_miners - 5));
+    if wallet.idlecoin > cost || wallet.supercoin > 0 {
+        miner::sub_idlecoins(wallet, cost);
+        wallet.max_miners += 1;
+    }
+
+    cost
 }
